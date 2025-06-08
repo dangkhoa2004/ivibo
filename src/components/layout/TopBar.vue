755b5155f188b5df5cb06532cea6a33cbfcf0fc0
dangkh0a <149858139+dangkhoa2004@users.noreply.github.com>
2025-06-08 12:34:52 +0700

    [UPDATE] ICON & TEMPLATE

--- src/components/layout/TopBar.vue
+++ src/components/layout/TopBar.vue
@@ -1,15 +1,20 @@
 <template>
-    <!-- Top bar -->
     <div class="flex items-center justify-between p-4">
-        <button>
+        <button aria-label="Mở tìm kiếm">
             <SearchIcon />
         </button>
+
         <div class="relative">
-            <NotificationIcon />
-            <span
-                class="absolute -top-2 -right-2 bg-orange-500 text-white text-xs rounded-full w-5 h-5 flex items-center justify-center">9</span>
+            <button aria-label="Xem thông báo">
+                <NotificationIcon />
+                <span
+                    class="absolute -top-2 -right-2 bg-orange-500 text-white text-xs rounded-full w-5 h-5 flex items-center justify-center">
+                    9
+                </span>
+            </button>
         </div>
     </div>
+
 </template>
 <script setup lang="ts">
 import SearchIcon from '@/components/icons/SearchIcon.vue'
