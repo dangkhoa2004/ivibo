6b5ffda7f26156c8f191a2ed280466f735b3a8ae
dangkh0a <149858139+dangkhoa2004@users.noreply.github.com>
2025-06-08 12:23:56 +0700

    [UPDATE] ICON & TEMPLATE

--- src/views/HomeView/HomeView.vue
+++ src/views/HomeView/HomeView.vue
@@ -1,23 +1,13 @@
 <template>
-  <div class="h-screen flex flex-col bg-white text-gray-800">
-    <!-- TopBar cố định -->
+  <div class="min-h-screen flex flex-col bg-white text-gray-800">
     <TopBar class="fixed top-0 left-0 right-0 z-20 bg-white" />
-
-    <!-- Nội dung cuộn -->
-    <div class="flex-1 overflow-y-auto pt-[56px] pb-[64px]"> <!-- padding để không bị đè -->
-      <!-- Banner -->
-      <div>
-        <img src="@/assets/img/banner.avif" alt="Banner" class="object-cover w-full h-full" />
+    <div class="flex-1 overflow-y-auto pt-[56px] pb-[64px]">
+      <div class="w-full aspect-[3/1] overflow-hidden">
+        <img src="@/assets/img/banner.avif" alt="Banner" class="w-full h-full object-cover" />
       </div>
-
-      <!-- Section: Lịch -->
       <CalendarSection />
-
-      <!-- Section: Time Set -->
       <TimeSetSection />
     </div>
-
-    <!-- BottomNavigation cố định -->
     <BottomNavigation class="fixed bottom-0 left-0 right-0 z-20 bg-white" />
   </div>
 </template>
--- src/views/HomeView/components/CalendarSection.vue
+++ src/views/HomeView/components/CalendarSection.vue
@@ -2,7 +2,7 @@
   <div class="mt-6 px-4">
     <div class="flex justify-between items-center mb-3">
       <h2 class="text-lg font-bold">Lịch</h2>
-      <button class="text-sm text-orange-500 font-medium">Xem thêm</button>
+      <button class="text-sm text-orange-700 font-medium">Xem thêm</button>
     </div>
 
     <div class="overflow-x-auto scrollbar-hide">
@@ -12,17 +12,18 @@
           <div class="rounded-2xl overflow-hidden bg-gray-200 w-50 shrink-0">
             <img src="@/assets/img/task.avif" alt="Công việc" class="w-full object-cover aspect-[4/3]" />
             <div class="p-2 space-y-0.5">
-              <p class="text-xs text-gray-500">QL. Công việc & dự án</p>
+              <p class="text-sm text-gray-700">QL. Công việc & dự án</p>
               <p class="text-base font-semibold">Công việc</p>
             </div>
           </div>
         </router-link>
+
         <!-- Card 2 -->
         <router-link to="/schedules">
           <div class="rounded-2xl overflow-hidden bg-gray-200 w-50 shrink-0">
             <img src="@/assets/img/schedule.avif" alt="Lịch trình" class="w-full object-cover aspect-[4/3]" />
             <div class="p-2 space-y-0.5">
-              <p class="text-xs text-gray-500">Theo dõi lịch trình</p>
+              <p class="text-sm text-gray-700">Theo dõi lịch trình</p>
               <p class="text-base font-semibold">Lịch trình</p>
             </div>
           </div>
